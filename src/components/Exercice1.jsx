d067c4d667b1ff1a1069fad7dcaeae394d7dfd92
RetrozDev <ryanbarbozapro@gmail.com>
2024-10-21 21:57:53 +0200

    Exercice 1 terminé

--- src/components/Exercice1.jsx
+++ src/components/Exercice1.jsx
@@ -1,24 +1,26 @@
+import { useState } from "react";
 import Exercice from "./container/Exercice";
 
 export default function Exercice1() {
-  // 1. Crée un state qui garde en mémoire la valeur du compteur.
-  // 2. Implémente une fonction pour incrémenter la valeur du compteur.
-  // 3. Implémente une fonction pour décrémenter la valeur du compteur.
-  // 4. Passe les fonctions d'incrémentation et de décrémentation aux boutons correspondants.
-  return (
-    <Exercice>
-      <h2>Exercice 1 : Compteur simple</h2>
-      <p className="instructions">
-        Modifie le composant Exercice1 pour faire fonctionner le compteur et ses
-        deux boutons.
-      </p>
-      <div className="solution">
-        <div>
-          <button>-</button>
-          <button>+</button>
-          <p>Compteur : 0</p>
-        </div>
-      </div>
-    </Exercice>
-  );
+	const [count, setCount] = useState(10);
+	return (
+		<Exercice>
+			<h2>Exercice 1 : Compteur simple</h2>
+			<p className="instructions">
+				Modifie le composant Exercice1 pour faire fonctionner le compteur et ses
+				deux boutons.
+			</p>
+			<div className="solution">
+				<div>
+					<button type="button" onClick={() => setCount(count - 1)}>
+						-
+					</button>
+					<button type="button" onClick={() => setCount(count + 1)}>
+						+
+					</button>
+					<p>Compteur : {count}</p>
+				</div>
+			</div>
+		</Exercice>
+	);
 }
