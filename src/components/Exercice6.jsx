21eef8939f2d1c78d78a012725fd2d30ffd83338
RetrozDev <ryanbarbozapro@gmail.com>
2024-10-21 22:56:14 +0200

    Exercice 6 terminé

--- src/components/Exercice6.jsx
+++ src/components/Exercice6.jsx
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import Exercice from "./container/Exercice";
 
 export default function Exercice6() {
-  return (
-    <Exercice>
-      <h2>Exercice 6 : Compteur avec limite</h2>
-      <p className="instructions">
-        Modifie le composant Exercice6 pour faire un compteur qui peut augmenter
-        jusqu'à une valeur maximale définie par l'utilisateur.
-      </p>
-      <div className="solution">
-        <div>
-          <input type="number" placeholder="Limite max" />
-          <p>Compteur : 0 </p>
-          <button> Augmenter </button>
-        </div>
-      </div>
-    </Exercice>
-  );
+	const [number, setNumber] = useState(0);
+	const [maxNumber, setMaxNumber] = useState(10);
+
+	return (
+		<Exercice>
+			<h2>Exercice 6 : Compteur avec limite</h2>
+			<p className="instructions">
+				Modifie le composant Exercice6 pour faire un compteur qui peut augmenter
+				jusqu'à une valeur maximale définie par l'utilisateur.
+			</p>
+			<div className="solution">
+				<div>
+					<input
+						type="number"
+						placeholder="Limite max :"
+						onChange={(e) => {
+							const value = Number(e.target.value);
+							if (!Number.isNaN(value) && value >= 0) {
+								setMaxNumber(value);
+							}
+						}}
+					/>
+					<p>Compteur : {number} </p>
+					<button
+						type="button"
+						onClick={() => {
+							if (number < maxNumber) {
+								setNumber(number + 1); 
+							}
+						}}
+					>
+						Augmenter
+					</button>
+				</div>
+			</div>
+		</Exercice>
+	);
 }
