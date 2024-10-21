670d5895ae96e8dc5c1cc5f54bab980ddea6c804
RetrozDev <ryanbarbozapro@gmail.com>
2024-10-21 23:12:33 +0200

    Exercice 7 terminé ✅

--- src/components/Exercice7.jsx
+++ src/components/Exercice7.jsx
@@ -1,30 +1,57 @@
+import { useState } from "react";
 import Exercice from "./container/Exercice";
 
 export default function Exercice7() {
-  // 1. Crée un state pour gérer les valeurs des champs du formulaire (nom, prénom, email)
-  // 2. Crée un state pour gérer les valeurs soumises
-  // 3. Crée une fonction pour mettre à jour les valeurs du formulaire à chaque changement dans les champs
-  // 4. Crée une fonction pour gérer la soumission du formulaire
+	// 1. Crée un state pour gérer les valeurs des champs du formulaire (nom, prénom, email)
+	// 2. Crée un state pour gérer les valeurs soumises
+	// 3. Crée une fonction pour mettre à jour les valeurs du formulaire à chaque changement dans les champs
+	// 4. Crée une fonction pour gérer la soumission du formulaire
+	const [formData, setFormData] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+	});
+	return (
+		<Exercice>
+			<h2>Exercice 7 : Formulaire multi-champs</h2>
+			<p className="instructions">
+				Modifie le composant Exercice7 pour faire un formulaire avec plusieurs
+				champs de texte contrôlés (nom, prénom, email) et affichez les valeurs
+				soumises en dessous du formulaire.
+			</p>
+			<div className="solution">
+				<div>
+					<input
+						type="text"
+						name="nom"
+						placeholder="Nom"
+						onChange={(e) =>
+							setFormData({ ...formData, lastName: e.target.value })
+						}
+					/>
 
-  return (
-    <Exercice>
-      <h2>Exercice 7 : Formulaire multi-champs</h2>
-      <p className="instructions">
-        Modifie le composant Exercice7 pour faire un formulaire avec plusieurs
-        champs de texte contrôlés (nom, prénom, email) et affichez les valeurs
-        soumises en dessous du formulaire.
-      </p>
-      <div className="solution">
-        <div>
-          <input type="text" name="nom" placeholder="Nom" />
-          <input type="text" name="prenom" placeholder="Prénom" />
-          <input type="email" name="email" placeholder="Email" />
-          <button>Save</button>
-        </div>
-        <p>Nom : </p>
-        <p>Prénom : </p>
-        <p>Email : </p>
-      </div>
-    </Exercice>
-  );
+					<input
+						type="text"
+						name="prenom"
+						placeholder="Prénom"
+						onChange={(e) =>
+							setFormData({ ...formData, firstName: e.target.value })
+						}
+					/>
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						onChange={(e) =>
+							setFormData({ ...formData, email: e.target.value })
+						}
+					/>
+					<button type="button" onClick={() => alert(`Merci ${formData.firstName}, maintenant nous pouvons revendre vos données mouahaha !`)}>Save</button>
+				</div>
+				<p>Nom : {formData.lastName} </p>
+				<p>Prénom : {formData.firstName}</p>
+				<p>Email : {formData.email}</p>
+			</div>
+		</Exercice>
+	);
 }
